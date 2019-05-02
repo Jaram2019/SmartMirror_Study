f070e324acfa849ab5e945eb2ef3af8ccd17edd0
Yeonggyun.Kim <kygha7205@naver.com>
2019-05-02 16:15:42 +0900

    Sample Node.js web server

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var bodyparser = require('body-parser');
+var underscore = require('underscore');
+
+app.locals.pretty = true;
+
+var PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended : false}));
+app.use(express.static('/public'));
+
+
+app.get('/', function(req, res){
+    res.send("Hello wolrd!");
+})
+
+app.listen(PORT, function(){
+    console.log(`Listening on ${PORT}`);
+});
