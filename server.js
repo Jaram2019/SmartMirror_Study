4eaeb7014d9082a0a4b506b8e8282ca119d8f366
Yeonggyun.Kim <kygha7205@naver.com>
2019-05-14 14:12:36 +0900

    Boostrap Initialized

--- server.js
+++ server.js
@@ -12,12 +12,11 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended : false}));
-app.use(express.static('/public'));
+app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', function(req, res){
-    res.send("Hello wolrd!");
-})
+app.use(['/','/index'], require('./routes/index')); //여러개의 라우팅을 한번에 : 배열에 담아서 선언.
+
 
 app.listen(PORT, function(){
     console.log(`Listening on ${PORT}`);
