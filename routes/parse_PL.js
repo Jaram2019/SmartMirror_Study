9311111bc39eb10b5413d7445c1e4feb042e4c0e
rdcpp <rdcpp.zzzz1362@gmail.com>
2019-11-30 13:13:24 +0900

    frontend updated

--- routes/parse_PL.js
+++ routes/parse_PL.js
@@ -1,47 +1,42 @@
+var router = require('express').Router();
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 
 const url = 'https://www.premierleague.com/tables';
 
-axios(url)
-  .then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const Table = $('.tableBodyContainer.isPL > tr');
-    const PLtable = [];
+router.get('/', function (req, res) {
+  axios(url)
+    .then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const Table = $('.tableBodyContainer.isPL > tr');
+      let PLtable = [];
 
-    Table.each(function () {
-      const position = $(this).find('.pos > .value').text();
-      const badge = $(this).find('.team > a > .badge-25').toString();
-      const club = $(this).find('.team > a > .long').text();
-      const club_short = $(this).find('.team > a > .short').text();
-      const points = $(this).find('.points').text();
-      const played = $(this).find('td:nth-child(4)').text();
-      const won = $(this).find('td:nth-child(5)').text();
-      const drown = $(this).find('td:nth-child(6)').text();
-      const lost  = $(this).find('td:nth-child(7)').text();
-      const gf  = $(this).find('td:nth-child(8)').text();
-      const ga  = $(this).find('td:nth-child(9)').text();
-      const gd  = $(this).find('td:nth-child(10)').text().trim();
+      Table.each(function () {
+        const position = $(this).find('.pos > .value').text();
+        const badge = $(this).find('.team > a > .badge-25').toString();
+        const club = $(this).find('.team > a > .long').text();
+        const club_short = $(this).find('.team > a > .short').text();
+        const points = $(this).find('.points').text();
+        const played = $(this).find('td:nth-child(4)').text();
+        const won = $(this).find('td:nth-child(5)').text();
+        const drawn = $(this).find('td:nth-child(6)').text();
+        const lost  = $(this).find('td:nth-child(7)').text();
+        const gf  = $(this).find('td:nth-child(8)').text();
+        const ga  = $(this).find('td:nth-child(9)').text();
+        const gd  = $(this).find('td:nth-child(10)').text().trim();
 
-      if(club !== ''){
-        const badgeArr = badge.split('"');
-        PLtable.push({
-          Position: position,
-          Badge: badgeArr[1],
-          Club: club,
-          Club_Short: club_short,
-          Played: played,
-          Won: won,
-          Drown: drown,
-          Lost: lost,
-          GF: gf,
-          GA: ga,
-          GD: gd,
-          Points: points,
-        });
-      }
-    });
-    console.log(PLtable);
-  })
-  .catch(console.error);
+        if(club !== ''){
+          PLtable.push([position,club,played,won,drawn,lost,gf,ga,gd,points])
+        }
+      });
+      res.render('parse_PL',{
+        PLtable : PLtable
+      });
+    })
+    .catch(console.error);
+});
+
+
+module.exports = router;
